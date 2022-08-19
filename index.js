0a2adf440564b1cafe36ed4790506c984bfb97dd
kevinIsomMoringa <kevin.isom@student.moringaschool.com>
2022-08-19 12:34:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 4
+const num4 = 6
+
+
+
+let multiply = num1*num2
+// equation that will multiply the variables num1 and num2; the result should be 62.
+
+let random = Math.floor(Math.random() * 10) + 1;
+// equation that will generate a random integer greater than 0.
+
+let mod = num3 % num4
+// equation that will calculate the remainder of dividing variable num3 by num4; /
+// the remainder should be 4.
+
+let max = Math.max(1, 2, 3, 4, 20, 1, 15, 5);
+// equation that finds the highest number in a set; the value returned should be 20.
+
+/*
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
+*/
